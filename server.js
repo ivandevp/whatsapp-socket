cf51df985ea38b44c5d66ac42f283c1841696461
Paola Ortiz de Zevallos <paolaozv@gmail.com>
2016-11-14 20:42:41 -0500

    contador de personas

--- server.js
+++ server.js
@@ -2,20 +2,57 @@ var express = require("express");
 var app = express();
 var server = require("http").createServer(app);
 var io = require("socket.io")(server);
+var port = process.env.PORT || 8086;
 
-var messages = [{}];
+server.listen(port, function() {
+	console.log("Servidor corriendo en", port);
+});
 
 app.use(express.static(__dirname + "/public"));
 
-io.on("connection", function(socket) {
-	console.log("Conectado");
-	socket.emit("messages", messages);
-	socket.on("new-message", function(data) {
-		messages.push(data);
-		io.sockets.emit("messages", messages);
-	});
-});
+// Chat
 
-server.listen(8086, function() {
-	console.log("Servidor corriendo en 8086");
+var numUsers = 0;
+
+io.on("connection", function(socket) {
+	var addedUser = false;
+
+	socket.on("new-message", function(data) {
+		socket.broadcast.emit("new-message", {
+			username: socket.username,
+			message: data
+		});
+	});
+
+	socket.on("add-user", function(username) {
+		if(addedUser) return;
+
+		socket.username = username;
+		++numUsers;
+		addedUser = true;
+		socket.emit("login", {
+			numUsers: numUsers
+		});
+
+		socket.broadcast.emit("user-joined", {
+			username: socket.username,
+			numUsers: socket.username
+		});
+	});
+
+	socket.on("stop-typing", function() {
+		socket.broadcast.emit("stop-typing", {
+			username: socket.username
+		});
+	});
+
+	socket.on("disconnect", function() {
+		if(addedUser) {
+			--numUsers;
+			socket.broadcast.emit("user-left", {
+				username: socket.username,
+				numUsers: numUsers
+			});
+		}
+	});
 });
