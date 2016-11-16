a57d45b1b9ceb59609df363a8db7d5af26d68cd9
anbelespinal <anabelespinal>
2016-11-15 20:47:09 -0500

    ultimos cambios

--- public/js/main.js
+++ public/js/main.js
@@ -1,66 +1,55 @@
+(function(d){
+   var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
+   if (d.getElementById(id)) {return;}
+   js = d.createElement('script'); js.id = id; js.async = true;
+   js.src = "//connect.facebook.net/en_US/all.js";
+   ref.parentNode.insertBefore(js, ref);
+ }(document));
 
-  // cargamos el sdk de forma asincrónica
-      (function(d){
-         var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
-         if (d.getElementById(id)) {return;}
-         js = d.createElement('script'); js.id = id; js.async = true;
-         js.src = "//connect.facebook.net/en_US/all.js";
-         ref.parentNode.insertBefore(js, ref);
-       }(document));
- 
-     // Iniciamos el sdk y su configuración
-      window.fbAsyncInit = function() {
-         FB.init({
-          appId      : "714817465348572", //la appid de tu aplicación facebook
-          status   : true,
-      cookie   : true,
-      xfbml    : true,
-      oauth    : true //enables OAuth 2.0
-        });
- 
-        //manejador para comprobar si el status del usuario ha cambiado o no 
-        FB.Event.subscribe('auth.statusChange', function(response) {
-          if (response.authResponse) {
-            //si el usuario es logueado correctamente le saludamos
-            FB.api('/me', function(me){
-              if (me.name) {
-								var foto = "<div class='avatar'>{{fotoprincipal}}</div>";
+window.fbAsyncInit = function() {
+   FB.init({
+    appId    : "714817465348572", //la appid de tu aplicación facebook
+    status   : true,
+		cookie   : true,
+		xfbml    : true,
+		oauth    : true //enables OAuth 2.0
+  });
 
-              	var plantilla = '<li>' + 
-		  														'<div id="contac">' + 
-											  						'<div>' + 
-														        	'<div id="foto" class="avatar">{{foto}}</div>' +
-											  							'<span id="saludo">{{saludo}}</span>' +
-											  						'</div>' +
-														        // '<a href="" id="salir">Cerrar Sesion</a>' +
-													        '</div>' + 				
-																'</li>' ;
+  FB.Event.subscribe('auth.statusChange', function(response) {
+    if (response.authResponse) {
+      FB.api('/me', function(me){
+        if (me.name) {
+					var foto = "<div class='avatar'>{{fotoprincipal}}</div>";
 
-								var agregrar ="";
+        	var plantilla = '<li>' + 
+														'<div class="contac">' + 
+								  						'<div>' + 
+											        	'<div class="foto avatar">{{foto}}</div>' +
+								  							'<span class="saludo">{{saludo}}</span>' +
+								  						'</div>' +
+										        '</div>' + 				
+													'</li>' ;
 
-								agregrar += plantilla.replace("{{saludo}}", me.name)
-														.replace("{{foto}}", '<img src="https://graph.facebook.com/' + me.id + '/picture">')
-								var a ="";
-								a = foto.replace("{{fotoprincipal}}", '<img src="https://graph.facebook.com/' + me.id + '/picture">');
-                // document.getElementById('saludo').innerHTML = me.name;
-                // document.getElementById('foto').innerHTML = 
-                // '<img src="https://graph.facebook.com/' + me.id + '/picture">';
+					var agregrar ="";
 
-                $("#fotoPrincipal").prepend(a);
-                $("#listaContactos").append(agregrar);
-                $("#borrar").css("display", "none");
-              }
-            })
-          } else {
-            //si el usuario no tiene permiso porque ha cerrado sesión o simplemente
-            //no tiene permisos de la aplicación redirigimos al login
-            window.location = "index.html";
-          }
-        });
-    //al pulsar en salir cerramos sesión y mandamos al inicio
-        document.getElementById('salir').addEventListener('click', function(e){
-        	e.preventDefault();
-          FB.logout();
-        }); 
-      } 
+					agregrar += plantilla.replace("{{saludo}}", me.name)
+											.replace("{{foto}}", '<img src="https://graph.facebook.com/' + me.id + '/picture">')
+					var a ="";
+					a = foto.replace("{{fotoprincipal}}", '<img src="https://graph.facebook.com/' + me.id + '/picture">');
+
+          $("#fotoPrincipal").prepend(a);
+          $("#listaContactos").append(agregrar);
+        }
+      })
+    } else {
+      window.location = "index.html";
+    }
+  });
+
+
+  document.getElementById('salir').addEventListener('click', function(e){
+  	e.preventDefault();
+    FB.logout();
+  }); 
+} 
 
--- server.js
+++ server.js
@@ -1,8 +1,11 @@
+// server
+
 var express = require("express");
 var app = express();
 var server = require("http").createServer(app);
 var io = require("socket.io")(server);
 
+// Heroku
 app.set("port", (process.env.PORT || 8086));
 
 app.use(express.static(__dirname + "/public"));
@@ -11,18 +14,56 @@ server.listen(app.get("port"), function() {
   console.log("Servidor corriendo en", app.get("port"));
 });
 
-// var messages = [{}];
-var messages = [{
-	id: 1,
-	text: "Hola Bienvenido a blaH",
-	author: ""
-}];
+//Hasta aquí Heroku
+
+// Chat
+
+var numUsers = 0;
 
 io.on("connection", function(socket) {
+	var addedUser = false;
 
-	socket.emit("messages", messages);
 	socket.on("new-message", function(data) {
-		messages.push(data);
-		io.sockets.emit("messages", messages);
+		socket.broadcast.emit("new-message", {
+			username: socket.username,
+			message: " " + data
+		});
+	});
+
+	socket.on("add-user", function(username) {
+		if(addedUser) return
+		socket.username = username;
+		++numUsers;
+		addedUser = true;
+		socket.emit("login", {
+			numUsers: numUsers
+		});
+
+		socket.broadcast.emit("user-joined", {
+			username: socket.username,
+			numUsers: socket.username
+		});
+	});
+
+	socket.on("typing", function () {
+    	socket.broadcast.emit("typing", {
+      		username: socket.username
+    	});
+  	});
+
+	socket.on("stop-typing", function() {
+		socket.broadcast.emit("stop-typing", {
+			username: socket.username
+		});
+	});
+
+	socket.on("disconnect", function() {
+		if(addedUser) {
+			--numUsers;
+			socket.broadcast.emit("user-left", {
+				username: socket.username,
+				numUsers: numUsers
+			});
+		}
 	});
 });
